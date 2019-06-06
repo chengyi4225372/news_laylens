d348a8de2b4f588c09c29bb92a44463fdc931c9b
chengyi <732345907@qq.com>
2019-06-06 14:24:04 +0800

    提交

---
+++ hunters0601.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50553
+Source Host           : 127.0.0.1:3306
+Source Database       : hunters
+
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
+
+Date: 2019-06-01 10:01:50
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
